ea0a34d1c3cb81df066fcaa1a5d058d6e48f80e4
Gyuwon <woni011420@gmail.com>
2022-03-25 10:23:03 +0900

    10-1. 미들웨어 기초 알아보기(2022년 3월 25일 TIL(개인공부))

--- src/08/store/configureStore.js
+++ src/08/store/configureStore.js
@@ -8,12 +8,57 @@
 
 ///09-3-1-4. 스토어 설정 파일 추가하기
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { SET_TRANSACTION_LIST } from '../actions/transactionActions';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from '../reducers';
+
+//10-1-1-2. 간단한 미들웨어 만들어 적용하기
+
+const customMiddleware = store => nextRunner => action => {
+    console.log('미들웨어에 전달된 액션 개체',action);//nextRunner() 함수 이전에 실행할 작업 1
+    console.log('리듀서 실행 전',store.getState());//nextRunner() 함수 이전에 실행할 작업 2
+    const result = nextRunner(action);
+    console.log('리듀서 실행 후',store.getState());
+    return result;
+}
+//10-1-2. 미들웨어로 액션 변형하기
+
+///10-1-2-1. 특정 액션의 payload 값 변경하기
+/*
+    액션의 type이 SET_TRANSACTION_LIST 인경우 payload의 값을 변경하는 미들웨어이다.
+*/
+
+const customMiddleware1 = () => nextRunner => action => {
+    if(action.type === SET_TRANSACTION_LIST){
+        return nextRunner({
+            ...action,
+            payload: [{
+                id:0,
+                code:'DOIT',
+                name:'두잇코인(DOIT)',
+                totalPrice: 1000000000,
+                currentPrice: 25000,
+                datetime: '현재시간',
+            }],
+        });
+    }
+
+    return nextRunner(action);
+}
+
+const customMiddleware2 = store => nextRunner => action => {
+    console.log('미들웨어2 전달된 액션 개체',action);
+    console.log('미들웨어2 실행 전',store.getState());
+    const result = nextRunner(action);
+    console.log('미들웨어2 살행 후',store.getState());
+    return result;
+}
 
 export default initStates => createStore(
     combineReducers(reducers),
     initStates,
-    composeWithDevTools(),
+    composeWithDevTools(
+        applyMiddleware(customMiddleware,customMiddleware2)
+    ),
 );
